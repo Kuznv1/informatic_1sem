6f792a8a2e02fb0a591a8087828edc69a59934f6
Kuznv1 <kuznetsova.vd@phystech.edu>
2025-02-28 17:01:43 +0300

    Dijkstra

---
+++ informatic/2_semestr/seminars/dijkstra.py
@@ -0,0 +1,29 @@
+G = {0: {1: 1.5, 4: 0.5}, 1:{2:1, 3:3}, 2:{3:1}, 3:{0:2}, 4: {1: 0.5}, 5:{4:1}}
+
+def Dijkstra(G,s):
+    V = len(G.keys())
+    dist = [float('inf') for i in range(V)]
+    prev = [None for i in range(V)]
+    dist[s] = 0
+    prev[s] = 0
+    S = set()
+    n = 0
+    while len(S) < V and n != V:
+        v = dist.index(min(dist))
+
+        S.add(v)
+        #dist[v] = float('inf')
+        for u, w in G[v].items():
+            if u not in S and dist[u] > dist[v] + w:
+                prev[u] = dist[v] + w
+                dist[u] = dist[v] + w                
+                #print(dist, v, u, S, n, prev)
+        dist[v] = float('inf')
+        n +=1
+
+        
+    return prev
+
+print(Dijkstra(G, 0))
+
+
